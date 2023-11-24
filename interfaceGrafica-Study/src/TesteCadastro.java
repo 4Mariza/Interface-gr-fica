fbe93af5984ab0a6841b8a7ae2a8dc3112121082
MARIA LUIZA GOMES DE ALMEIDA <23111819@CFP127ALUNO.LOCAL>
2023-11-24 08:54:03 -0300

    first commit

---
+++ interfaceGrafica-Study/src/TesteCadastro.java
@@ -0,0 +1,9 @@
+
+public class TesteCadastro {
+
+	public static void main(String[] args) {
+		
+		new Cadastro();
+	}
+
+}
---
+++ interfaceGrafica-Study/src/TesteImc.java
@@ -0,0 +1,9 @@
+
+public class TesteImc {
+
+	public static void main(String[] args) {
+		new Imc();
+
+	}
+
+}
